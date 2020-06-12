dd60013fc5c3cffc0f716827fdd9c2d2a56f51d3
Daniel Dickson <fluroclad@gmail.com>
2020-06-12 17:38:00 +0100

    database test

---
+++ api/database.py
@@ -0,0 +1,24 @@
+import psycopg2
+try:
+    connection = psycopg2.connect(  user = "postgres",
+                                    password = "admin",
+                                    host = "localhost",
+                                    port = "5432",
+                                    database = "postgres")
+
+    c = connection.cursor()
+
+    print(connection.get_dsn_parameters(), "\n")
+
+    c.execute("SELECT version();")
+    record = c.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except(Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+
+finally:
+    if(connection):
+        c.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
